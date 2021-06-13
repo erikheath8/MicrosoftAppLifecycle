cb3ca03a10f235e01a911dd3949daf6c096c462f
Erik Heath <erikheath8@gmail.com>
2021-06-12 22:25:06 -0400

    first & last commit of Microsoft's app activiity lifecyle tutorial

---
+++ App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static System.Diagnostics.Debug;
+
+namespace AppLifecycleTutorial
+{
+    public partial class App : Application
+    {
+        const string displayText = "displayText";
+
+        public string DisplayText { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            WriteLine("OnStart");
+
+            if (Properties.ContainsKey(displayText))
+            {
+                DisplayText = (string)Properties[displayText];
+            }
+        }
+
+        protected override void OnSleep()
+        {
+            WriteLine("OnSleep");
+            Properties[displayText] = DisplayText;
+        }
+
+        protected override void OnResume()
+        {
+            WriteLine("OnResume");
+        }
+    }
+}
